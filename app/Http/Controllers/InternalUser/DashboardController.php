36d9618ee5e1a53d86f7af102724cdd1ba677f30
TahmidFarzan <tfarzan007@gmail.com>
2022-11-29 09:05:23 +0600

    Mode dashbord to internal user.

--- app/Http/Controllers/InternalUser/DashboardController.php
+++ app/Http/Controllers/InternalUser/DashboardController.php
@@ -14,6 +14,6 @@ class DashboardController extends Controller
 
     public function index()
     {
-        return view('pages.dashboard.index');
+        return view('internal user.dashboard.index');
     }
 }
