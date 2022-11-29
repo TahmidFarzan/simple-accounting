e05dba4c8c6cbffca099a96cd0118838d1438cfd
TahmidFarzan <tfarzan007@gmail.com>
2022-11-29 14:54:44 +0600

    Index sorting and create user logic add.

--- app/Http/Controllers/InternalUser/UserController.php
+++ app/Http/Controllers/InternalUser/UserController.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 use App\Utilities\SystemConstant;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-
 
 class UserController extends Controller
 {
@@ -28,9 +28,9 @@ class UserController extends Controller
 
     public function index(Request $request)
     {
-        $pagination = 1;
+        $pagination = 5;
 
-        $paginations = array(1,5,15,30,45,60,75,90,100);
+        $paginations = array(5,15,30,45,60,75,90,100);
         $searchFields = array("All","Name","Email","Mobile no");
 
         $activeUsers = User::orderby("name","desc");
@@ -60,14 +60,14 @@ class UserController extends Controller
 
             if(($request->has('search_field') && ($request->has('search') && !($request->search_field == null)) && !($request->search == null))){
 
-                $searchCustomerField = (in_array( $request->search_field,$searchFields)) ? Str::lower(str_replace(" ","_",$request->search_field) ): null;
+                $searchUserField = (in_array( $request->search_field,$searchFields)) ? Str::lower(str_replace(" ","_",$request->search_field) ): null;
 
-                if(!($searchCustomerField == null)){
+                if(!($searchUserField == null)){
                     switch ($request->selected_nav_tab) {
                         case "Active":
                             $activeUsers = $activeUsers->whereNotNull("id");
 
-                            switch ($searchCustomerField) {
+                            switch ($searchUserField) {
                                 case "all":
                                     $activeUsers = $activeUsers->orWhere("name","like","%".$request->search."%")
                                                                     ->orWhere("email","like","%".$request->search."%")
@@ -75,7 +75,7 @@ class UserController extends Controller
                                 break;
 
                                 default:
-                                    $activeUsers = $activeUsers->where($searchCustomerField,"like","%".$request->search."%");
+                                    $activeUsers = $activeUsers->where($searchUserField,"like","%".$request->search."%");
                                 break;
                             }
                         break;
@@ -83,7 +83,7 @@ class UserController extends Controller
                         case "Trash":
                             $trashUsers = $trashUsers->whereNotNull("id");
 
-                            switch ($searchCustomerField) {
+                            switch ($searchUserField) {
                                 case "all":
                                     $trashUsers = $trashUsers->orWhere("name","like","%".$request->search."%")
                                                                     ->orWhere("email","like","%".$request->search."%")
@@ -91,7 +91,7 @@ class UserController extends Controller
                                 break;
 
                                 default:
-                                    $trashUsers = $trashUsers->where($searchCustomerField,"like","%".$request->search."%");
+                                    $trashUsers = $trashUsers->where($searchUserField,"like","%".$request->search."%");
                                 break;
                             }
                         break;
@@ -113,5 +113,120 @@ class UserController extends Controller
     public function details($slug){
         $user = User::withTrashed()->where("slug",$slug)->firstOrFail();
         return view('internal user.user.details',compact("user"));
+    }
+
+    public function create(){
+        $userRoles = array();
+
+        if(Auth::user()->hasUserPermission(["UMP02"]) == true){
+            array_push($userRoles,"Owner");
+        }
+
+        if(Auth::user()->hasUserPermission(["UMP03"]) == true){
+            array_push($userRoles,"Subordinate");
+        }
+
+        return view('internal user.user.create',compact("userRoles"));
+    }
+
+    public function save(Request $request){
+        $validator = Validator::make($request->all(),
+            [
+                'name' => 'required|max:200',
+                'mobile_no' => 'nullable|max:20|regex:/^([0-9\s\-\+]*)$/|unique:users,mobile_no',
+                'email' => 'required|email|max:255|unique:users,email',
+                'user_role' => 'required|in:Owner,Subordinate',
+                'auto_email_verify' => 'required|in:Yes,No',
+                'default_password' => 'required|in:Yes,No',
+                'password' => 'nullable|required_if:default_password,No|max:255',
+            ],
+            [
+                'name.required' => 'Name is required.',
+                'name.max' => 'Name length can not greater then 200 chars.',
+
+                'mobile_no.max' => 'Mobile no length can not greater then 20 chars.',
+                'mobile_no.regex' => 'Mobile no must be mobile no.',
+                'mobile_no.unique' => 'Mobile no must be unique.',
+
+                'email.max' => 'Email length can not greater then 255 chars.',
+                'email.email' => 'Email must be email.',
+                'email.unique' => 'Email must be unique.',
+
+                'user_role.required' => 'User role is reqired.',
+                'user_role.in' => 'User role must one out of [Owner,Subordinate].',
+
+                'auto_email_verify.required' => 'Auto email verify is reqired.',
+                'auto_email_verify.in' => 'Auto email verify must one out of [Yes,No].',
+
+                'default_password.required' => 'Default password is reqired.',
+                'default_password.in' => 'Default password must one out of [Yes,No].',
+
+                'password.required_if' => 'Password is reqired.',
+                'password.max' => 'Password length can not greater then 255 chars..',
+            ]
+        );
+
+        $validator->after(function ($validator) {
+            $afterValidatorData = $validator->getData();
+
+            if($afterValidatorData["user_role"] == "Owner"){
+
+                if(Auth::user()->hasUserPermission(["UMP02"]) == false){
+                    $validator->errors()->add(
+                        'user_role', "Your do not have permission to create user that have 'Owner' user role."
+                    );
+                }
+            }
+
+            if($afterValidatorData["user_role"] == "Subordinate"){
+
+                if(Auth::user()->hasUserPermission(["UMP03"]) == false){
+                    $validator->errors()->add(
+                        'user_role', "Your do not have permission to create user that have 'Subordinate' user role."
+                    );
+                }
+            }
+
+        });
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $statusInformation = array("status" => "errors","message" => array());
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->mobile_no = $request->mobile_no;
+            $user->email = $request->email;
+            $user->user_role = $request->user_role;
+            $user->email_verified_at = ($request->auto_email_verify == "Yes") ? Carbon::now() : null;
+            $user->password = Hash::make(($request->default_password == "Yes") ? "123456789" : $request->password);
+            $user->default_password = ($request->default_password == "Yes") ? 1 : 0;
+            $user->slug = SystemConstant::slugGenerator($request->name,200);
+            $user->created_at = Carbon::now();
+            $user->created_by_id = Auth::user()->id;
+            $user->updated_at = null;
+            $saveUser = $user->save();
+
+        if($saveUser){
+            $statusInformation["status"] = "status";
+            array_push($statusInformation["message"],"User successfully created.");
+
+            if($request->auto_email_verify == "No"){
+                $user->sendEmailVerificationNotification();
+                array_push($statusInformation["message"],"Please asked user to verify email before is expired.");
+            }
+
+            if($request->default_password == "Yes"){
+                array_push($statusInformation["message"],"Default pasword(123456789) is save for user password.");
+            }
+        }
+        else{
+            $statusInformation["status"] = "errors";
+            $statusInformation["message"] = "Fail to save user.";
+        }
+
+        return redirect()->route("user.index")->with([$statusInformation["status"] => $statusInformation["message"]]);
     }
 }
---
+++ resources/views/internal user/user/create.blade.php
@@ -0,0 +1,254 @@
+@extends('layouts.app')
+
+@section('mainPageName')
+    Customer
+@endsection
+
+@section('mainCardTitle')
+    Add
+@endsection
+
+@section('navBreadcrumbSection')
+    <nav aria-label="breadcrumb" class="ms-3">
+        <ol class="breadcrumb m-1 mb-2">
+            <li class="breadcrumb-item"><a href="{{ route("user.index") }}">Customer</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Add</li>
+        </ol>
+    </nav>
+@endsection
+
+@section('authContentOne')
+    <div class="card border-dark mb-2">
+        <div class="card-body text-dark">
+            <form action="{{ route("user.save") }}" method="POST" id="addForm">
+                @csrf
+
+                <div class="form-group mb-3">
+                    <div class="row">
+                        <div class="col-md-6 mb-2">
+                            <div class="row">
+                                <label class="col-lg-4 col-form-label col-form-label-sm text-bold">Name <i class="fa-solid fa-asterisk" style="font-size: 10px;!important"></i></label>
+                                <div class="col-lg-8">
+                                    <input type="text" id="nameInput" name="name" class="form-control form-control-sm @error('name') is-invalid @enderror" value="{{ old('name') }}" placeholder="Ex: Hello" maxlength="200" required>
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert" style="display: block;">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 mb-2">
+                            <div class="row">
+                                <label class="col-lg-4 col-form-label col-form-label-sm text-bold">User role <i class="fa-solid fa-asterisk" style="font-size: 10px;!important"></i></label>
+                                <div class="col-lg-8">
+                                    <select class="form-control form-control-sm @error('user_role') is-invalid @enderror" id="userRoleInput" name="user_role" required>
+                                        <option value="">Select</option>
+                                        @foreach ($userRoles as $perUserRole)
+                                            <option value="{{ $perUserRole }}" {{ (old('user_role') == $perUserRole) ? 'selected' : null }}>{{ $perUserRole }}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('user_role')
+                                        <span class="invalid-feedback" role="alert" style="display: block;">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-6 mb-2">
+                            <div class="row">
+                                <label class="col-lg-4 col-form-label col-form-label-sm text-bold">Email <i class="fa-solid fa-asterisk" style="font-size: 10px;!important"></i></label>
+                                <div class="col-lg-8">
+                                    <input type="email" id="emailInput" name="email" class="form-control form-control-sm @error('email') is-invalid @enderror" value="{{ old('email') }}" placeholder="Ex: hello@xx.com" maxlength="255" required>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert" style="display: block;">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 mb-2">
+                            <div class="row">
+                                <label class="col-lg-4 col-form-label col-form-label-sm text-bold">Mobile no </label>
+                                <div class="col-lg-8">
+                                    <input type="text" id="mobileNoInput" name="mobile_no" class="form-control form-control-sm @error('mobile_no') is-invalid @enderror" value="{{ old('mobile_no') }}" placeholder="Ex: 16XXXXXXX" maxlength="20" required>
+                                    <div class="invalid-feedback" id="mobileNoInputIntlTelInputExtraError" style="display: none;"></div>
+                                    @error('mobile_no')
+                                        <span class="invalid-feedback" role="alert" style="display: block;">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 mb-2">
+                            @php
+                                $currentAutoEmailVerifyOption = (old("auto_email_verify") == null) ? "No" : old("auto_email_verify");
+                            @endphp
+
+                            <div class="row">
+                                <label class="col-lg-4 col-form-label col-form-label-sm text-bold">Auto email verify <i class="fa-solid fa-asterisk" style="font-size: 10px;!important"></i></label>
+                                <div class="col-lg-8">
+                                    <div class=" pt-2">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="auto_email_verify" id="autoEmailVerifyOptionYes" value="Yes" {{ ($currentAutoEmailVerifyOption == "Yes") ? "checked" : null }}>
+                                            <label class="form-check-label">Yes</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="auto_email_verify" id="autoEmailVerifyOptionNo" value="No" {{ ($currentAutoEmailVerifyOption == "No") ? "checked" : null }}>
+                                            <label class="form-check-label">No</label>
+                                        </div>
+                                    </div>
+
+                                    @error('auto_email_verify')
+                                        <span class="invalid-feedback" role="alert" style="display: block;">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 mb-2">
+                            @php
+                                $currentDefaultPasswordOption = (old("default_password") == null) ? "Yes" : old("default_password");
+                            @endphp
+
+                            <div class="row">
+                                <label class="col-lg-4 col-form-label col-form-label-sm text-bold">Default password <i class="fa-solid fa-asterisk" style="font-size: 10px;!important"></i></label>
+                                <div class="col-lg-8">
+                                    <div class="pt-2">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="default_password" id="defaultPasswordOptionYes" value="Yes" {{ ($currentDefaultPasswordOption == "Yes") ? "checked" : null }}>
+                                            <label class="form-check-label">Yes</label>
+                                        </div>
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="default_password" id="defaultPasswordOptionNo" value="No" {{ ($currentDefaultPasswordOption == "No") ? "checked" : null }}>
+                                            <label class="form-check-label">No</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="mb-2">
+                                        <input type="password" id="passwordInput" name="password" class="form-control form-control-sm @error('password') is-invalid @enderror" value="{{ old('password') }}" placeholder="Enter your password." maxlength="255"  @if($currentDefaultPasswordOption == "No") required @endif @if($currentDefaultPasswordOption == "Yes") readonly hidden @endif>
+                                    </div>
+
+                                    @error('default_password')
+                                        <span class="invalid-feedback" role="alert" style="display: block;">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert" style="display: block;">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mb-0">
+                    <div class="col-md-8 offset-md-4 mb-3">
+                        <button type="submit" class="btn btn-outline-success">
+                            Save
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@section('authContentTwo')
+    <div class="card border-dark mb-3">
+        <div class="card-body">
+            <div class="d-flex justify-content-center">
+                <a role="button" href="{{ route("user.index") }}" class="btn btn-sm btn-secondary">
+                    Go to customer
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('onPageExtraScript')
+    <script src="{{ asset("intlTelInput/intlTelInput.min.js") }}"></script>
+    <script src="{{ asset("intlTelInput/intlTelInput-jquery.min.js") }}"></script>
+    <script>
+        $(document).ready(function(){
+            var formErrorCount=0;
+            var errorMap = [ "invalid number", "invalid country code", "too short", "too long", "Invalid number"];
+            var mobileNoInput = document.querySelector("#mobileNoInput");
+
+            var mobileNoInputPlugin = window.intlTelInput(mobileNoInput,({
+                allowDropdown: true,
+                autoPlaceholder: "aggressive",
+                autoHideDialCode: false,
+                customPlaceholder: null,
+                dropdownContainer: null,
+                excludeCountries: [],
+                formatOnDisplay: true,
+                hiddenInput: "",
+                initialCountry: "",
+                localizedCountries: "",
+                nationalMode: false,
+                placeholderNumberType: "MOBILE",
+                preferredCountries: ["BD"],
+                separateDialCode: false,
+                utilsScript: "{{ asset('intlTelInput/utils.js') }}",
+            }));
+
+            mobileNoInput.addEventListener('blur', function() {
+                if(mobileNoInput.value.trim()){
+                    if(mobileNoInputPlugin.isValidNumber()){
+                        $('#mobileNoInputIntlTelInputExtraError').html(null);
+                        $('#mobileNoInputIntlTelInputExtraError').css("display","none");
+                        if(formErrorCount>0){
+                            formErrorCount=formErrorCount-1;
+                        }
+                    }
+                    else{
+                        formErrorCount=formErrorCount+1;
+                        $('#mobileNoInputIntlTelInputExtraError').css("display","block");
+                        $('#mobileNoInputIntlTelInputExtraError').html('<strong>'+"Mobile no is "+errorMap[mobileNoInputPlugin.getValidationError()]+'</strong>');
+                    }
+                }
+            });
+
+            $("#addForm").submit(function(e){
+                if(formErrorCount > 0){
+                    e.preventDefault();
+                }
+                else{
+                    return true;
+                }
+            });
+
+            $(document).on('change','input[type=radio][name=default_password]', function () {
+                if($(this).val() == "Yes"){
+                    $("#passwordInput").prop("hidden",true);
+                    $("#passwordInput").prop("readonly",true);
+                    $("#passwordInput").prop("required",false);
+                }
+                else{
+                    $("#passwordInput").prop("hidden",false);
+                    $("#passwordInput").prop("readonly",false);
+                    $("#passwordInput").prop("required",true);
+                }
+            });
+        });
+    </script>
+@endpush
+
+@push('onPageExtraCss')
+    <link href="{{ asset("intlTelInput/intlTelInput.min.css") }}" rel="stylesheet">
+@endpush
