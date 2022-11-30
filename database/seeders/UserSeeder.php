49a0e18e4c339e6efa3805077cc617e757434fbf
TahmidFarzan <tfarzan007@gmail.com>
2022-11-30 15:10:47 +0600

    User seeder update.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,13 +2,10 @@
 
 namespace Database\Seeders;
 
-use Carbon\Carbon;
 use App\Models\User;
-use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use App\Utilities\SystemConstant;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -16,6 +13,7 @@ class UserSeeder extends Seeder
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         User::truncate();
+        DB::table('user_permission_group_has_user_permissions')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         $tahmidFarzanUser=User::factory()->state([
@@ -23,7 +21,7 @@ class UserSeeder extends Seeder
             'email' => "tfarzan007@gmail.com",
             'mobile_no' => '+8801671786285',
             'created_by_id' => 1,
-            'user_role'=>"Owner", // Owner||Subordinate
+            'user_role'=> "Owner", // Owner||Subordinate
             'slug' => SystemConstant::slugGenerator("Tahmid Farzan",200),
         ])->create();
     }
