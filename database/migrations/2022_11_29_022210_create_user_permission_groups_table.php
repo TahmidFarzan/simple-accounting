1a7d3269ebc98d26c51a338d5e8bd865b49b433a
TahmidFarzan <tfarzan007@gmail.com>
2022-11-30 21:27:47 +0600

    user permission group table.

--- database/migrations/2022_11_29_022210_create_user_permission_groups_table.php
+++ database/migrations/2022_11_29_022210_create_user_permission_groups_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('user_permission_groups', function (Blueprint $table) {
             $table->id();
             $table->string('name',100);
-            $table->string('code',100);
+            $table->string('code',100)->unique();
             $table->string('slug',255)->unique();
             $table->unsignedBigInteger('created_by_id');
             $table->timestamps();
