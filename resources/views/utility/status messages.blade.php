69c14285e3682d4f0bb4a9e9ef2a2ab7b04159ce
TahmidFarzan <tfarzan007@gmail.com>
2022-12-01 09:36:31 +0600

    Status message layout update.

--- resources/views/utility/status messages.blade.php
+++ resources/views/utility/status messages.blade.php
@@ -16,13 +16,13 @@
         <div class="alert-messages alert alert-danger" role="alert">
             <div class="row">
                 <div class="col-11 col-lg-11 col-md-11 col-sm-11">
-                    @if (is_string(Session::get('errors'))==true)
+                    @if (is_string(Session::get('errors')) == true)
                         <p class="text-center">
                             <b>{{ Session::get('errors') }}</b>
                         </p>
                     @endif
 
-                    @if ((is_string(Session::get('errors'))==false))
+                    @if ((is_string(Session::get('errors')) == false))
                         <ul>
                             @foreach ($errors->all() as $perError)
                                 <li>{{ $perError }}</li>
@@ -42,12 +42,12 @@
     <div class="alert-messages alert alert-success" role="alert">
         <div class="row">
             <div class="col-11 col-lg-11 col-md-11 col-sm-11">
-                @if (is_string(Session::get('status'))==true)
+                @if (is_string(Session::get('status')) == true)
                     <p class="text-center">
                         <b>{{ Session::get('status') }}</b>
                     </p>
                 @endif
-                @if ((is_string(Session::get('status'))==false))
+                @if ((is_string(Session::get('status')) == false))
                     <ul>
                         @foreach (Session::get('status') as $perStatus)
                             <li>{{ $perStatus }}</li>
@@ -66,12 +66,12 @@
     <div class="alert-messages alert alert-warning" role="alert">
         <div class="row">
             <div class="col-11 col-lg-11 col-md-11 col-sm-11">
-                @if (is_string(Session::get('warning'))==true)
+                @if (is_string(Session::get('warning')) == true)
                     <p class="text-center">
                         <b>{{ Session::get('warning') }}</b>
                     </p>
                 @endif
-                @if ((is_string(Session::get('warning'))==false))
+                @if ((is_string(Session::get('warning')) == false))
                     <ul>
                         @foreach (Session::get('warning') as $perWarning)
                             <li>{{ $perWarning }}</li>
