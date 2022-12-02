3672ea5dd7658e3f3c19945614a4727fa5f7d043
TahmidFarzan <tfarzan007@gmail.com>
2022-12-02 10:51:41 +0600

    User table update.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name',200);
-            $table->string('mobile_no',15)->nullable()->unique();
+            $table->string('mobile_no',20)->nullable()->unique();
             $table->string('email',255)->unique();
             $table->enum('user_role', ['Owner','Subordinate'])->default('Subordinate');
             $table->string('slug',255)->unique();
