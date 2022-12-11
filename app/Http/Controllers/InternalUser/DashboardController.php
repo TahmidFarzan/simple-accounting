1bc7c3fa8cdad35071fa77bbbe53a9a01121fac2
TahmidFarzan <tfarzan007@gmail.com>
2022-12-11 14:22:15 +0600

    Update dashboard for PC quick view.

--- app/Http/Controllers/InternalUser/DashboardController.php
+++ app/Http/Controllers/InternalUser/DashboardController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\InternalUser;
 
 use Illuminate\Http\Request;
 use App\Models\ProjectContract;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\ProjectContractJournal;
+use App\Models\ProjectContractPayment;
 
 class DashboardController extends Controller
 {
@@ -15,7 +18,44 @@ class DashboardController extends Controller
 
     public function index(Request $request)
     {
-        $projectContracts = ProjectContract::orderBy("created_at","desc")->orderBy("name","asc")->get();
-        return view('internal user.dashboard.index',compact("projectContracts"));
+        $currentDate = now();
+        $startDate = $currentDate->startOfMonth()->format('Y-m-d');
+        $endDate = $currentDate->endOfMonth()->format('Y-m-d');
+
+
+        $projectContractJournalEntries = ProjectContractJournal::orderBy("created_at","desc")->orderBy("name","asc")->get();
+        $projectContractPayments = ProjectContractPayment::orderBy("created_at","desc")->orderBy("name","asc")->get();
+
+        $ongoingProjectContractCount = ProjectContract::where("status","Ongoing")
+                                                        ->where(DB::raw("(STR_TO_DATE(start_date,'%Y-%m-%d'))"),'>=', $startDate)
+                                                        ->where(DB::raw("(STR_TO_DATE(start_date,'%Y-%m-%d'))"),'<=', $endDate)
+                                                        ->count();
+
+
+        $completeProjectContractCount = ProjectContract::where("status","Complete")
+                                                        ->where(DB::raw("(STR_TO_DATE(end_date,'%Y-%m-%d'))"),'>=', $startDate)
+                                                        ->where(DB::raw("(STR_TO_DATE(end_date,'%Y-%m-%d'))"),'<=', $endDate)
+                                                        ->count();
+
+        $revenueAmountProjectContractJournal = ProjectContractJournal::where("entry_type","Revenue")
+                                                        ->where(DB::raw("(STR_TO_DATE(entry_date,'%Y-%m-%d'))"),'>=', $startDate)
+                                                        ->where(DB::raw("(STR_TO_DATE(entry_date,'%Y-%m-%d'))"),'<=', $endDate)
+                                                        ->sum("amount");
+
+
+        $lossAmountProjectContractJournal = ProjectContractJournal::where("entry_type","Loss")
+                                                        ->where(DB::raw("(STR_TO_DATE(entry_date,'%Y-%m-%d'))"),'>=', $startDate)
+                                                        ->where(DB::raw("(STR_TO_DATE(entry_date,'%Y-%m-%d'))"),'<=', $endDate)
+                                                        ->sum("amount");
+        $paymentProjectContractPayment = ProjectContractPayment::where(DB::raw("(STR_TO_DATE(payment_date,'%Y-%m-%d'))"),'>=', $startDate)
+                                                            ->where(DB::raw("(STR_TO_DATE(payment_date,'%Y-%m-%d'))"),'<=', $endDate)
+                                                            ->sum("amount");
+
+        $projectContractQuickView = array("ongoing" => $ongoingProjectContractCount,"complete" => $completeProjectContractCount);
+        $projectContractJournalQuickView = array("revenue" => $revenueAmountProjectContractJournal,"loss" => $lossAmountProjectContractJournal);
+        $projectContractPaymentQuickView = array("payment" => $paymentProjectContractPayment);
+
+
+        return view('internal user.dashboard.index',compact("projectContractQuickView","projectContractJournalQuickView","projectContractPaymentQuickView"));
     }
 }
--- resources/views/internal user/dashboard/index.blade.php
+++ resources/views/internal user/dashboard/index.blade.php
@@ -21,93 +21,41 @@
 @endsection
 
 @section('authContentOne')
-
     @php
-        $currentDate = now();
-        $currentDateFormat = now()->format('Y-m-d');
-        $currentWeekStartDate = $currentDate->startOfWeek()->format('Y-m-d');
-        $currentWeekEndDate = $currentDate->endOfWeek()->format('Y-m-d');
-        $currentMonthStartDate = $currentDate->startOfMonth()->format('Y-m-d');
-        $currentMonthEndDate = $currentDate->endOfMonth()->format('Y-m-d');
-        $currentYearStartDate = $currentDate->startOfYear()->format('Y-m-d');
-        $currentYearEndDate = $currentDate->endOfYear()->format('Y-m-d');
+        $projectContractPopOver = "<p>";
+        $projectContractPopOver = $projectContractPopOver."<b>Ongoing :</b> ".$projectContractQuickView["ongoing"]."</br>";
+        $projectContractPopOver = $projectContractPopOver."<b>Complete :</b> ".$projectContractQuickView["complete"]."</br>";
+        $projectContractPopOver = $projectContractPopOver."<b>Total :</b> ".($projectContractQuickView["ongoing"] + $projectContractQuickView["complete"])."</br>";
+        $projectContractPopOver = $projectContractPopOver."</p>";
 
-        $previousDate = now()->subDays(1);
-        $previousDateFormat = now()->subDays(1)->format('Y-m-d');
-        $previousWeekStartDate = $previousDate->startOfWeek()->format('Y-m-d');
-        $previousWeekEndDate = $previousDate->endOfWeek()->format('Y-m-d');
-        $previousMonthStartDate = $previousDate->startOfMonth()->format('Y-m-d');
-        $previousMonthEndDate = $previousDate->endOfMonth()->format('Y-m-d');
-        $previousYearStartDate = $previousDate->startOfYear()->format('Y-m-d');
-        $previousYearEndDate = $previousDate->endOfYear()->format('Y-m-d');
+        $projectContractJournalPopOver = "<p>";
+        $projectContractJournalPopOver = $projectContractJournalPopOver."<b>Revenue :</b> ".$projectContractJournalQuickView["revenue"]." ".$setting["businessSetting"]["currency_symbol"]."</br>";
+        $projectContractJournalPopOver = $projectContractJournalPopOver."<b>Loss :</b> ".$projectContractJournalQuickView["loss"]." ".$setting["businessSetting"]["currency_symbol"]."</br>";
+        $projectContractJournalPopOver = $projectContractJournalPopOver."<b>Balance :</b> ".($projectContractJournalQuickView["revenue"] - $projectContractJournalQuickView["loss"])." ".$setting["businessSetting"]["currency_symbol"]."</br>";
+        $projectContractJournalPopOver = $projectContractJournalPopOver."</p>";
 
     @endphp
+
     <div class="card border-dark mb-3">
         <div class="card-body text-dark">
-            <h5 class="card-title">Project contract quick view</h5>
-        </div>
-        <div class="card-body text-dark">
-            <nav>
-                <div class="nav nav-tabs" id="projectContractQuickViewNavTabList" role="tablist">
-                    <button class="nav-link active" id="projectContractQuickViewNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewNavContent" type="button" role="tab" aria-controls="projectContractQuickViewNavContent" aria-selected="true">Project contract</button>
-                    <button class="nav-link" id="patnershipBusinessNavTab" data-bs-toggle="tab" data-bs-target="#patnershipBusinessNavContent" type="button" role="tab" aria-controls="patnershipBusinessNavContent" aria-selected="false">Patnership Business</button>
-                </div>
-            </nav>
-            <div class="tab-content" id="projectContractQuickViewNavTabContent">
-                <div class="tab-pane fade show active" id="projectContractQuickViewNavContent" role="tabpanel" aria-labelledby="projectContractQuickViewNavTab" tabindex="0">
-                    <div class="card-body">
-                        <nav>
-                            <div class="nav nav-tabs" id="projectContractQuickViewNavGroupTab" role="tablist">
-                                <button class="nav-link active" id="projectContractQuickViewCurrentNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewCurrentNavContent" type="button" role="tab" aria-controls="projectContractQuickViewCurrentNavContent" aria-selected="true">Current</button>
-                                <button class="nav-link" id="projectContractQuickViewPreviousNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewPreviousNavContent" type="button" role="tab" aria-controls="projectContractQuickViewPreviousNavContent" aria-selected="false">Previous</button>
-                            </div>
-                        </nav>
+            <div class="d-flex justify-content-center">
+                <h5 class="card-title">Project contract quick view</h5>
+            </div>
 
-                        <div class="tab-content" id="projectContractQuickViewNavGroupTabContent">
-                            <div class="tab-pane fade show active" id="projectContractQuickViewCurrentNavContent" role="tabpanel" aria-labelledby="projectContractQuickViewCurrentNavTab" tabindex="0">
-                                <div class="card-body">
-                                    <nav>
-                                        <div class="nav nav-tabs" id="projectContractQuickViewCurrentGroupTab" role="tablist">
-                                            <button class="nav-link active" id="projectContractQuickViewCurrentDateNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewCurrentDateNavContant" type="button" role="tab" aria-controls="projectContractQuickViewCurrentDateNavContant" aria-selected="true">Day</button>
-                                            <button class="nav-link" id="projectContractQuickViewCurrentWeekNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewCurrentWeekNavContent" type="button" role="tab" aria-controls="projectContractQuickViewCurrentWeekNavContent" aria-selected="false">Week</button>
-                                            <button class="nav-link" id="projectContractQuickViewCurrentMonthNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewCurrentMonthNavContent" type="button" role="tab" aria-controls="projectContractQuickViewCurrentMonthNavContent" aria-selected="false">Month</button>
-                                            <button class="nav-link" id="projectContractQuickViewCurrentYearNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewCurrentYearNavContent" type="button" role="tab" aria-controls="projectContractQuickViewCurrentYearNavContent" aria-selected="false">Year</button>
-                                        </div>
-                                    </nav>
-                                    <div class="tab-content" id="projectContractQuickViewCurrentGroupTabContent">
-                                        <div class="tab-pane fade show active" id="projectContractQuickViewCurrentDateNavContant" role="tabpanel" aria-labelledby="projectContractQuickViewCurrentDateNavTab" tabindex="0">
-                                            <div class="card-body">
+            <div class="row">
+                <div class="card-body">
+                    <button type="button" class="btn btn-sm btn-primary col-md-4 m-1" data-bs-container="body" data-bs-animation="true" data-bs-html="true" data-bs-toggle="popover" data-bs-trigger="focus"  data-bs-placement="top" data-bs-custom-class="project-contract-popover" data-bs-title="Receivable amount information" data-bs-content="{{ $projectContractPopOver }}">
+                        <b>Total project contract: </b> {{ $projectContractQuickView["ongoing"] + $projectContractQuickView["complete"] }}
+                    </button>
 
-                                            </div>
-                                        </div>
-                                        <div class="tab-pane fade" id="projectContractQuickViewCurrentWeekNavContent" role="tabpanel" aria-labelledby="projectContractQuickViewCurrentWeekNavTab" tabindex="0">...</div>
-                                        <div class="tab-pane fade" id="projectContractQuickViewCurrentMonthNavContent" role="tabpanel" aria-labelledby="projectContractQuickViewCurrentMonthNavTab" tabindex="0">...</div>
-                                        <div class="tab-pane fade" id="projectContractQuickViewCurrentYearNavContent" role="tabpanel" aria-labelledby="projectContractQuickViewCurrentYearNavTab" tabindex="0">...</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="tab-pane fade" id="projectContractQuickViewPreviousNavContent" role="tabpanel" aria-labelledby="projectContractQuickViewPreviousNavTab" tabindex="0">
-                                <div class="card-body">
-                                    <nav>
-                                        <div class="nav nav-tabs" id="projectContractQuickViewPreviousGroupTab" role="tablist">
-                                            <button class="nav-link active" id="projectContractQuickViewPreviousDateNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewPreviousDateNavContant" type="button" role="tab" aria-controls="projectContractQuickViewPreviousDateNavContant" aria-selected="true">Day</button>
-                                            <button class="nav-link" id="projectContractQuickViewPreviousWeekNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewPreviousWeekNavContent" type="button" role="tab" aria-controls="projectContractQuickViewPreviousWeekNavContent" aria-selected="false">Week</button>
-                                            <button class="nav-link" id="projectContractQuickViewPreviousMonthNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewPreviousMonthNavContent" type="button" role="tab" aria-controls="projectContractQuickViewPreviousMonthNavContent" aria-selected="false">Month</button>
-                                            <button class="nav-link" id="projectContractQuickViewPreviousYearNavTab" data-bs-toggle="tab" data-bs-target="#projectContractQuickViewPreviousYearNavContent" type="button" role="tab" aria-controls="projectContractQuickViewPreviousYearNavContent" aria-selected="false">Year</button>
-                                        </div>
-                                    </nav>
-                                    <div class="tab-content" id="projectContractQuickViewPreviousGroupTabContent">
-                                        <div class="tab-pane fade show active" id="projectContractQuickViewPreviousDateNavContant" role="tabpanel" aria-labelledby="projectContractQuickViewPreviousDateNavTab" tabindex="0">...</div>
-                                        <div class="tab-pane fade" id="projectContractQuickViewPreviousWeekNavContent" role="tabpanel" aria-labelledby="projectContractQuickViewPreviousWeekNavTab" tabindex="0">...</div>
-                                        <div class="tab-pane fade" id="projectContractQuickViewPreviousMonthNavContent" role="tabpanel" aria-labelledby="projectContractQuickViewPreviousMonthNavTab" tabindex="0">...</div>
-                                        <div class="tab-pane fade" id="projectContractQuickViewPreviousYearNavContent" role="tabpanel" aria-labelledby="projectContractQuickViewPreviousYearNavTab" tabindex="0">...</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <button type="button" class="btn btn-sm btn-primary col-md-4 m-1" data-bs-container="body" data-bs-animation="true" data-bs-html="true" data-bs-toggle="popover" data-bs-trigger="focus"  data-bs-placement="top" data-bs-custom-class="project-contract-popover" data-bs-title="Receivable amount information" data-bs-content="{{ $projectContractJournalPopOver }}">
+                        <b>Total journal balance :</b>  {{ $projectContractJournalQuickView["revenue"] - $projectContractJournalQuickView["loss"] }} {{ $setting["businessSetting"]["currency_symbol"] }}
+                    </button>
+
+                    <button type="button" class="btn btn-sm btn-primary col-md-4 m-1" >
+                        <b>Total payment :</b>  {{ $projectContractPaymentQuickView["payment"] }} {{ $setting["businessSetting"]["currency_symbol"] }}
+                    </button>
                 </div>
-                <div class="tab-pane fade" id="patnershipBusinessNavContent" role="tabpanel" aria-labelledby="patnershipBusinessNavTab" tabindex="0">...</div>
             </div>
         </div>
     </div>
@@ -121,3 +69,45 @@
         </div>
     </div>
 @endsection
+
+
+@push("onPageExtraScript")
+    <script>
+        $(document).ready(function(){
+            const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+            const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+        });
+    </script>
+@endpush
+
+
+@push('onPageExtraCss')
+    <style>
+        .project-contract-popover {
+            --bs-popover-max-width: auto;
+            --bs-popover-border-color: var(--bs-primary);
+            --bs-popover-header-bg: var(--bs-primary);
+            --bs-popover-header-color: var(--bs-white);
+            --bs-popover-body-padding-x: 1rem;
+            --bs-popover-body-padding-y: .5rem;
+        }
+
+        .project-contract-journal-popover {
+            --bs-popover-max-width: auto;
+            --bs-popover-border-color: var(--bs-primary);
+            --bs-popover-header-bg: var(--bs-primary);
+            --bs-popover-header-color: var(--bs-white);
+            --bs-popover-body-padding-x: 1rem;
+            --bs-popover-body-padding-y: .5rem;
+        }
+
+        .project-contract-journal-payment {
+            --bs-popover-max-width: auto;
+            --bs-popover-border-color: var(--bs-primary);
+            --bs-popover-header-bg: var(--bs-primary);
+            --bs-popover-header-color: var(--bs-white);
+            --bs-popover-body-padding-x: 1rem;
+            --bs-popover-body-padding-y: .5rem;
+        }
+    </style>
+@endpush
