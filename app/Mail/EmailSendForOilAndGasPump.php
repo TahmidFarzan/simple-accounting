e28ea21bb58509f239e787f15cf166903d6ba848
TahmidFarzan <tfarzan007@gmail.com>
2022-12-30 16:09:45 +0600

    Email temple and email logic for OilAndGasPump

---
+++ app/Mail/EmailSendForOilAndGasPump.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use App\Models\OilAndGasPump;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Contracts\Queue\ShouldQueue;
+class EmailSendForOilAndGasPump extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct($event,$envelope,$subject,OilAndGasPump $oilAndGasPump)
+    {
+        $this->event = $event;
+        $this->subject = $subject;
+        $this->envelope = $envelope;
+        $this->oilAndGasPump = $oilAndGasPump;
+    }
+
+    public function envelope()
+    {
+        $sendCCs = array();
+        $sendTos = array();
+        $sendReplys = array();
+
+        $sendFrom = User::where("email",$this->envelope["from"])->firstOrFail();
+
+        foreach(User::where("email",$this->envelope["to"])->get() as $perTo){
+            array_push($sendTos,new Address($perTo->email,$perTo->name));
+        }
+
+        foreach(User::where("email",$this->envelope["cc"])->get() as $perCC){
+            array_push($sendCCs,new Address($perCC->email,$perCC->name));
+        }
+
+        foreach(User::where("email",$this->envelope["reply"])->get() as $perReply){
+            array_push($sendReplys,new Address($perReply->email,$perReply->name));
+        }
+
+        $sendFrom =  new Address($sendFrom->email,$sendFrom->name);
+
+        return new Envelope(
+            to: $sendTos,
+            cc: $sendCCs,
+            from: $sendFrom,
+            replyTo: $sendReplys,
+            subject: $this->subject,
+        );
+    }
+
+    public function content()
+    {
+        return new Content(
+            view: 'internal user.oil and gas pump.oil and gas pump.email',
+            with: [
+                'oilAndGasPump' => $this->oilAndGasPump,
+                'event' => $this->event,
+            ],
+        );
+    }
+
+    public function attachments()
+    {
+        return [];
+    }
+}
