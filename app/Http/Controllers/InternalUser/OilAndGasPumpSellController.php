76583b2bd007b29b468f35bf74eac274976e8dbb
TahmidFarzan <tfarzan007@gmail.com>
2023-02-17 23:00:19 +0600

    OAGP Sell controller

---
+++ app/Http/Controllers/InternalUser/OilAndGasPumpSellController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\InternalUser;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OilAndGasPumpSellController extends Controller
+{
+    //
+}
